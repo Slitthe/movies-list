949304b01d117441a03954c35affab602ccbef8e
Slitthe <sil.gherman@gmail.com>
2023-11-23 22:22:07 +0200

    stringify

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -8,7 +8,7 @@ export default async function handler(
     res: NextApiResponse<ConfigApi>,
 ) {
 
-    console.log({ body: req.body });
+    console.log({ body: JSON.stringify(req.body) });
 
 
     res.status(200).json({
